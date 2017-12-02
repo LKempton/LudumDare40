6c14dd6f5e1172f7648ac8835637e5332d001560
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-12-02 10:33:44 +0000

    Player movement now works as intended.

--- LudumDare40/Assets/Scripts/PlayerMovement.cs
+++ LudumDare40/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [SerializeField]
+    private CircleCollider2D col;
+
+    private bool isOverPlayer = false;
+
     private void OnMouseOver()
     {
         if (Input.GetMouseButton(0))
         {
+            isOverPlayer = true;
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
         }    
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            isOverPlayer = false;
+        }
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+
+        float radius = col.radius;
+
+        float difference = Vector3.Distance(transform.position, mousePos);
+
+        if (Input.GetMouseButton(0) && (difference > radius) && isOverPlayer) 
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        }
     }
 }
