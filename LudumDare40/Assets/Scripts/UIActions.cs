ad06c77cba37bdf64d855e2c372a6b652053e2b5
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-12-02 15:13:06 +0000

    Made a basic Main Menu

---
+++ LudumDare40/Assets/Scripts/UIActions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIActions:MonoBehaviour {
+
+	public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
