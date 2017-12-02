0d2f5771381e678bdb448a5a7176549bc92dd9b1
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-12-02 10:05:02 +0000

    Basic player movement is in. Still needs tweaking as it can be buggy.

---
+++ LudumDare40/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        }    
+    }
+}
