b50ff959ee2566e08ff44411af4524c26b5b5850
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-12-02 11:07:35 +0000

    added cook movement

---
+++ LudumDare40/Assets/Scripts/CooksMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooksMovement : MonoBehaviour {
+    [SerializeField]
+    float speed = 5, movementRate = 6, randomRange =2;
+   
+    Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(Move());
+	}
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            transform.Rotate(0, 0, Random.Range(0, 359.999f));
+            rbody.velocity = transform.right * speed;
+            yield return new WaitForSeconds(movementRate + Random.RandomRange(-0.5f*randomRange,0.5f*randomRange));
+        }
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    // Update is called once per frame
+
+}
