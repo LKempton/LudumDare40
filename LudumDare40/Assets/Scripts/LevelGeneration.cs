cb900bc72368c193a8d5609e88748fcef2378092
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-12-02 11:48:46 +0000

    Started creating nodes for the levels. Added a colour pulse effect to each node.

---
+++ LudumDare40/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] firstNodes;
+    [SerializeField]
+    private GameObject[] secondNodes;
+    [SerializeField]
+    private GameObject[] thirdNodes;
+}
---
+++ LudumDare40/Assets/Scripts/OverlayPulse.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayPulse : MonoBehaviour {
+
+    [SerializeField]
+    private SpriteRenderer overlay;
+
+    [SerializeField]
+    private Color start;
+    [SerializeField]
+    private Color end;
+
+    [SerializeField]
+    private float pulseSpeed;
+
+    private void OnEnable()
+    {
+        StartCoroutine(ColourPulse(pulseSpeed));
+    }
+
+
+    IEnumerator ColourPulse(float duration)
+    {
+        bool isRising = true;
+
+        while (true)
+        {
+            if (isRising)
+            {
+                float t = 0;
+
+                while (t < 1)
+                {
+                    overlay.color = Color.Lerp(start, end, t);
+                    t += Time.deltaTime / duration;
+                    yield return null;
+                }
+
+                isRising = false;
+            }
+            else if (!isRising)
+            {
+                float t = 0;
+
+                while (t < 1)
+                {
+                    overlay.color = Color.Lerp(end, start, t);
+                    t += Time.deltaTime / duration;
+                    yield return null;
+                }
+
+                isRising = true;
+            }
+
+ 
+        }
+
+
+    }
+}
