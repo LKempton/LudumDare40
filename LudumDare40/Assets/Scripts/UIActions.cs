25606200b636de535c46607794f4930b9fcc971c
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-12-03 11:27:39 +0000

    Added a basic tutorial on the main menu and put a panel around the gameover text.

--- LudumDare40/Assets/Scripts/UIActions.cs
+++ LudumDare40/Assets/Scripts/UIActions.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class UIActions:MonoBehaviour {
+public class UIActions : MonoBehaviour {
 
-	public void LoadMainScene()
+    [SerializeField]
+    private GameObject panel;
+
+    private bool isOpen = false;
+
+    public void LoadMainScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ShowTutorial()
+    {
+        if (isOpen)
+        {
+            panel.SetActive(false);
+            isOpen = false;
+        }
+        else if (!isOpen)
+        {
+            panel.SetActive(true);
+            isOpen = true;
+        }
     }
 }
